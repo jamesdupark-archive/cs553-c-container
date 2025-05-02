268c61387f03061b47ad35f3ba9a7a302cd36838
James M. Park <jmp747@ilab2.cs.rutgers.edu>
2025-05-02 02:02:48 -0400

    feat: switched from fork to clone and added namespace isolations

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # cs553-c-container
 
 A C-based container system mimicking the functionalities of Linux's native `unshare` command.
+
+# How to Run
+Meant to be run on the cs553 course VM. Simply run
+```
+make clean all
+```
+which will generate the execuable file `ccontainer` for you. Run the container as follows:
+```
+./ccontainer <flags>
+```
+The container executable accepts the following flags:
--- container.c
+++ container.c
@@ -4,15 +4,59 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/mount.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sched.h>
 // #include
 
+#define CONTAINER_MEMORY 4096
 
-// int create_container(void *args) {
+int create_container(void *args) {
+    // char *args[] = {"/bin/chroot", ".", "sh", NULL};
+    char *av[] = {"/bin/sh", NULL};
 
-// }
+    // set pgid and take terminal control
+    int pid = getpid();
+    if (setpgid(pid, pid) < 0) {
+        perror("setpgid");
+        exit(1);
+    }
+
+    // printf("child %d : %d, parent %d\n", pid, getpgid(pid), getppid());
+    signal(SIGTTOU, SIG_IGN);
+    if (tcsetpgrp(STDIN_FILENO, pid) < 0) {
+        perror("tcsetpgrp");
+        exit(1);
+    }
+    // if (tcsetpgrp(STDOUT_FILENO, pid) < 0) {
+    //     perror("tcsetpgrp");
+    //     exit(1);
+    // }
+    // if (tcsetpgrp(STDERR_FILENO, pid) < 0) {
+    //     perror("tcsetpgrp");
+    //     exit(1);
+    // }
+
+    // check if system image is present
+    struct stat *astat = malloc(sizeof(struct stat));
+    if (stat("alpine", astat) < 0) { // TODO: download and extract image, flags for image URL/location
+        perror("stat");
+    }
+    free(astat);
+    chdir("alpine");
+    chroot(".");
+    mount("proc", "proc", "proc", 0, NULL);
+
+    // mount system image
+
+    // mount proc
+    
+    execv("/bin/sh", av);
+    perror("execv");
+    exit(1);
+}
 
 
 int main(int argc, char **argv) {
@@ -22,38 +66,21 @@ int main(int argc, char **argv) {
 
     }
 
+    int clone_flags = CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWNET; // pfmn
+
     // start new shell process
     pid_t pid;
-    if ((pid = fork()) == 0) { // TODO: check for errors
-        char *av[] = {"/usr/bin/sh", NULL};
-        // char *arr[] = {"/bin/echo", "hello", NULL};
-        pid = getpid();
-        signal(SIGTTOU, SIG_IGN);
-        printf("pgid: %d\n", getpgid(pid));
-        if (setpgid(pid, pid) < 0) {
-            perror("setpgid");
-            exit(1);
-        } // TODO: check for errors
-        printf("child %d : %d, parent %d\n", pid, getpgid(pid), getppid());
-        if (tcsetpgrp(STDIN_FILENO, pid) < 0) {
-            perror("tcsetpgrp");
-            exit(1);
-        } // TODO: check for errors
-        // if (tcsetpgrp(STDOUT_FILENO, pid) < 0) {
-        //     perror("tcsetpgrp");
-        //     exit(1);
-        // } // TODO: check for errors
-        execv("/usr/bin/sh", av);
-        perror("execv");
+    void *stack = malloc(CONTAINER_MEMORY);
+    if ((pid = clone(create_container, stack + CONTAINER_MEMORY, clone_flags, NULL)) < 0) {
+        perror("clone");
         exit(1);
     }
     sleep(2);
-    printf("child %d, parent %d : %d\n", pid, getpid(), getpgid(getpid()));
-    // char *arr[] = {"/usr/bin/sh", NULL};
-    // execv("/usr/bin/sh", arr);
+    // printf("child %d, parent %d : %d\n", pid, getpid(), getpgid(getpid()));
 
     int status = 0;
     wait(&status);
+    // free(stack);
 
     return 0;
 }
